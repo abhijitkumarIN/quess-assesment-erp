bfb7031402cac73fc0be4758711af7e312dd9ee7
abhjitkumarIN <abhijeetkumarlucknow@gmail.com>
2026-03-15 10:34:29 +0530

    fix: build issue

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"],
     });
+
     return config;
   },
-  env:{
+
+  env: {
     API_URL: process.env.API_URL,
   },
-    turbopack: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
+
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
       },
     },
-  
+  },
 };
 
 export default nextConfig;
